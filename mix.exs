31d2b1ed8ecafd6f0aae50919077d5c35dc71d68
sushant12 <sussyoung9@gmail.com>
2022-05-08 16:35:52 +0545

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FindAlias.MixProject do
     [
       app: :find_alias,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
